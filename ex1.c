ffde72cb285276fe7eff7b17e8dff0643716a392
Michał Kuśmierczyk <kusm@protonmail.ch>
2015-04-24 15:51:54 +0200

    Done exercises 1 - 5

---
+++ ex1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    puts("Hello world.");
+    puts("What's up");
+    puts("look at that");
+    puts("nope");
+    puts("THIS\n"
+         "IS"
+         "SOME"
+         "TEXT");
+
+    return 0;
+}
